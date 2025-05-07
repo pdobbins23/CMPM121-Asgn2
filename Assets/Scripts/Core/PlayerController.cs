0627e405875ab92199aa7b3e3c8932f097b62463
pdobbins23 <146784496+pdobbins23@users.noreply.github.com>
2025-05-07 01:44:02 -0700

    Small fix

--- Assets/Scripts/Core/PlayerController.cs
+++ Assets/Scripts/Core/PlayerController.cs
@@ -28,8 +28,10 @@ public class PlayerController : MonoBehaviour
     public void StartLevel()
     {
         spellcaster = new SpellCaster(125, 8, Hittable.Team.PLAYER);
+        
         Spell startingSpell = new Spell(SpellManager.Instance.AllSpells["arcane_bolt"], spellcaster);
-        spellcaster.spell = startingSpell;
+        
+        spellcaster.spells.Add(startingSpell);
         
         StartCoroutine(spellcaster.ManaRegeneration());
         
